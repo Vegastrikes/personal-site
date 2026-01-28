3df05d99c92af0702328b4b10e0311ded4c7eb22
Ozerk Ozay <ozerkozay@hotmail.com>
2026-01-28 22:31:44 +0300

    add cards for this site and 1 other project

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -2,10 +2,36 @@ import type { Project } from "../types/project";
 
 export const projects: Project[] = [
   {
+    title: "This Site",
+    frontend: ["Vue 3", "Pinia", "TypeScript", "Tailwind",],
+    backend: ["Docker"],
+    features: ["Mobile Friendly", "Themes"],
+    linkDemo: "",
+    linkSource: "https://github.com/Vegastrikes/personal-site",
+    info: [
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+    ]
+  },
+  {
+    title: "Template Site Engineering Firm",
+    frontend: ["HTML", "Tailwind",],
+    backend: ["PHP Plain"],
+    features: ["Mobile Friendly", "Multiple Language Support", "Server Side Rendering - SSR", "Fast Response", "SEO Friendly"],
+    linkDemo: "https://template-site.ozerkozay.dev/",
+    linkSource: "https://github.com/Vegastrikes/template-site",
+    info: [
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+    ]
+  },
+  {
     title: "SaaS Dashboard",
     frontend: ["Vue 3", "Pinia", "TypeScript", "Tailwind",],
     backend: ["Node", "Express", "Postgres", "Docker"],
-    features: ["Auth", "JWT", "CRUD", "Route Guards"],
+    features: ["Mobile Friendly", "Auth", "JWT", "CRUD", "Route Guards"],
     linkDemo: "https://saas-dashboard.ozerkozay.dev/",
     linkSource: "https://github.com/Vegastrikes/saas_dashboard",
     info: [
